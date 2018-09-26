9eb8aede51d5fdf6e40518e3e11c33c611e30ac8
Ian Kent <email@iankent.co.uk>
2018-09-26 20:32:44 +0100

    reload from UI, regex for expected value

--- main.go
+++ main.go
@@ -7,7 +7,8 @@ import (
 	"log"
 	"net/http"
 	"os/exec"
-	"strings"
+	"regexp"
+	"strconv"
 
 	"github.com/gorilla/pat"
 	yaml "gopkg.in/yaml.v2"
@@ -30,11 +31,14 @@ type step struct {
 
 	OK     bool   `json:"ok"`
 	Actual string `json:"actual"`
+	Error  string `json:"error"`
 }
 
 var d = &doc{}
 
 func loadDocs() {
+	d = &doc{}
+
 	b, err := ioutil.ReadFile("example/doc.yml")
 	if err != nil {
 		panic(err)
@@ -51,35 +55,52 @@ func loadDocs() {
 }
 
 func checkState() {
-	var completed int
+	for _, v := range d.Steps {
+		checkStepState(v)
+	}
+}
+
+func checkStepState(v *step) {
+	var originalOK = v.OK
 	defer func() {
-		d.CompletedSteps = completed
+		if originalOK && !v.OK {
+			d.CompletedSteps--
+		} else if !originalOK && v.OK {
+			d.CompletedSteps++
+		}
 	}()
 
-	for _, v := range d.Steps {
-		log.Printf("Testing [%s]", v.Test)
+	log.Printf("Testing [%s]", v.Test)
 
-		cmd := exec.Command("bash", "-c", v.Test)
+	cmd := exec.Command("bash", "-c", v.Test)
 
-		b, err := cmd.CombinedOutput()
+	b, err := cmd.CombinedOutput()
 
-		if err != nil {
-			log.Printf("Error [%s]: %s", v.Test, err)
-			v.Actual = err.Error()
-			continue
-		}
-
-		v.Actual = string(b)
-
-		if !strings.HasPrefix(v.Actual, v.Expected) {
-			log.Printf("NOT OK [%s]: Expected [%s], Actual [%s]", v.Test, v.Expected, v.Actual)
-			continue
-		}
-
-		log.Printf("OK [%s]: Expected [%s], Actual [%s]", v.Test, v.Expected, v.Actual)
-		v.OK = true
-		completed++
+	if err != nil {
+		log.Printf("Error [%s]: %s", v.Test, err)
+		v.Error = err.Error()
+		v.OK = false
+		return
 	}
+
+	v.Actual = string(b)
+
+	expectedRe, err := regexp.Compile(v.Expected)
+	if err != nil {
+		log.Printf("INVALID REGEX [%s]: %s", v.Test, v.Expected)
+		v.Error = fmt.Sprintf("Invalid regex for expected output: %s", v.Expected)
+		v.OK = false
+		return
+	}
+
+	if !expectedRe.MatchString(v.Actual) {
+		log.Printf("NOT OK [%s]: Expected [%s], Actual [%s]", v.Test, v.Expected, v.Actual)
+		v.OK = false
+		return
+	}
+
+	log.Printf("OK [%s]: Expected [%s], Actual [%s]", v.Test, v.Expected, v.Actual)
+	v.OK = true
 }
 
 func main() {
@@ -87,6 +108,19 @@ func main() {
 	checkState()
 
 	p := pat.New()
+
+	p.Options("/reload", func(w http.ResponseWriter, req *http.Request) {
+		w.Header().Set("Content-Type", "application/json")
+		w.Header().Set("Access-Control-Allow-Origin", "*")
+	})
+
+	p.Post("/reload", func(w http.ResponseWriter, req *http.Request) {
+		// TODO maybe make this asynchronous, and add a lock
+		loadDocs()
+		checkState()
+		w.Header().Set("Content-Type", "application/json")
+		w.Header().Set("Access-Control-Allow-Origin", "*")
+	})
 
 	p.Get("/steps", func(w http.ResponseWriter, req *http.Request) {
 		jsonb, err := json.Marshal(&d)
@@ -97,6 +131,24 @@ func main() {
 		w.Header().Set("Content-Type", "application/json")
 		w.Header().Set("Access-Control-Allow-Origin", "*")
 		w.Write(jsonb)
+	})
+
+	p.Post("/steps/{id}/retest", func(w http.ResponseWriter, req *http.Request) {
+		stepID := req.URL.Query().Get(":id")
+		nID, err := strconv.Atoi(stepID)
+		if err != nil {
+			w.WriteHeader(404)
+			return
+		}
+
+		if nID < 0 || nID > len(d.Steps)-1 {
+			w.WriteHeader(404)
+			return
+		}
+
+		checkStepState(d.Steps[nID])
+		w.Header().Set("Content-Type", "application/json")
+		w.Header().Set("Access-Control-Allow-Origin", "*")
 	})
 
 	if err := http.ListenAndServe(":5555", p); err != nil {
--- web-ui/src/App.js
+++ web-ui/src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-import {Badge, Col, Row, Collapsible, CollapsibleItem, ProgressBar} from 'react-materialize';
+import {Badge, Col, Row, Collapsible, CollapsibleItem, ProgressBar, Button} from 'react-materialize';
 
 class Step extends Component {
   constructor(props) {
@@ -22,7 +22,7 @@ class Step extends Component {
   }
 
   itemHeader(step) {
-    return <span>{step.title}<Badge>{step.ok ? "OK" : "Not OK"}</Badge></span>;
+    return <span>{step.title}<Badge>{step.ok ? "OK" : (step.error ? "Not installed" : "Wrong version") }</Badge></span>;
   }
 
   render() {
@@ -32,8 +32,16 @@ class Step extends Component {
       <p><tt>{this.props.step.test}</tt></p>
       <h4>What you should get</h4>
       <p><tt>{this.props.step.expected}</tt></p>
-      <h4>What you actually get</h4>
-      <p><tt>{this.props.step.actual}</tt></p>
+      <div>
+        <h4>What you actually get</h4>
+        <p><tt>{this.props.step.actual ? this.props.step.actual : <i>Nothing</i>}</tt></p>
+      </div>
+      { this.props.step.error ? 
+        <div>
+          <h4>This might be why</h4>
+          <p><tt>{this.props.step.error}</tt></p>
+        </div>
+      : "" }
       <h3>How to install it</h3>
       <p><tt>{this.props.step.install}</tt></p>
     </CollapsibleItem>
@@ -49,20 +57,41 @@ class App extends Component {
         steps: [],
         total_steps: 0,
         completed_steps: 0
-      }
+      },
+      loading: false
     };
+    this.handleReload = this.handleReload.bind(this);
+    this.fetchStepData = this.fetchStepData.bind(this);
+  }
+
+  fetchStepData() {
+    this.setState({loading:true});
+    fetch("http://localhost:5555/steps")
+    .then(r => r.json())
+    .then(json => {
+      console.log(json);
+      this.setState({doc: json, loading:false});
+    })
+    .catch(e => {
+      console.log(e);
+    });
+  }
+
+  handleReload() {
+    this.setState({loading:true});
+    fetch("http://localhost:5555/reload", {
+      method: "POST"
+    })
+    .then(r => {
+      this.fetchStepData();
+    })
+    .catch(e => {
+      console.log(e);
+    });
   }
   
   componentDidMount() {
-    fetch("http://localhost:5555/steps")
-      .then(r => r.json())
-      .then(json => {
-        console.log(json);
-        this.setState({doc: json});
-      })
-      .catch(e => {
-        console.log(e);
-      });
+    this.fetchStepData();
   }
 
   render() {
@@ -82,6 +111,9 @@ class App extends Component {
             <p>
               { this.state.doc.completed_steps } of { this.state.doc.total_steps } steps completed
             </p>
+            <div>
+              <Button disabled={this.state.loading} onClick={this.handleReload}>Reload</Button>
+            </div>
           </div>
         </header>
         <Row>
